1d7cad2af5e07e3134f8bb1535955ad0b4c4888b
Simone Odegard <Simone.Odegard@gmail.com>
2021-02-12 15:03:50 -0800

    added sum and multiply for problem 3

--- starter-code/app.js
+++ starter-code/app.js
@@ -57,10 +57,26 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 
+    let sumFirstTwoNumbers = sum (a,b);
+    let sumPrepForThree = sum (sumFirstTwoNumbers[0], c);
+    let sumFinalThree = sumPrepForThree[0];
+    console.log(sumFinalThree);
+
+    let sumThreeWords = a + ' and ' + b + ' and ' + c + ' sum to ' + sumFinalThree + '.';
+
+    let multiplyFirstTwo = multiply(a,b);
+    let multiplyPrep = multiply (multiplyFirstTwo[0], c);
+    let multiplyFinalThree = multiplyPrep[0];
+    console.log(multiplyFinalThree);
+
+    let multiplyThreeWords = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multiplyFinalThree + '.';
+
+    return [sumFinalThree, multiplyFinalThree, sumThreeWords, multiplyThreeWords];
+
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
