a9e66d246d6c6fb6eea6d7fbe9d51b4c32a1c6c1
jeremywbrazell <jwbrazell@gmail.com>
2021-02-12 14:29:34 -0800

    adding problem 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,34 +1,41 @@
 'use strict';
-/////////////////////////////////////
-/* Problem 1 (this is your demo that we'll solve in class)
-Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
+// /////////////////////////////////////
+// /* Problem 1 (this is your demo that we'll solve in class)
+// Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
 
-"The sum of 4 and 7 is 11."
-
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
-
-// Write your code here
-function sum(a, b) { //eslint-disable-line
-
+// "The sum of 4 and 7 is 11."
+function sum(a, b) {
+return [ a + b ,'The sum of ' + a + ' and ' + b + ' is ' + (a+b) + '.'];
+// Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 }
+// // Write your code here
+testSum(4,7); //eslint-disable-line
 
-// Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+// }
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+// // Here is the test for sum(); uncomment it to run it
+// // testSum(4, 7);
 
-/////////////////////////////////////
-/* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+// // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
-"The product of 5 and 9 is 45."
+// /////////////////////////////////////
+// /* Problem 2
+// Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
-// Write your code here
-function multiply(a, b) { //eslint-disable-line
 
-}
+// function [functionTwo, 'The product of 5 and 9 is 45']{
+// let functionTwo = a * b;
+// }
+// functionTwo();
+// // "The product of 5 and 9 is 45."
+
+// // Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+
+// // Write your code here
+// function multiply(a, b) { //eslint-disable-line
+
+// }
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
