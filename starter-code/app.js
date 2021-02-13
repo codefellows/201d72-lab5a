4c02cf79156f6f8f391fb523cc7d3abc63ee6adf
jeremywbrazell <jwbrazell@gmail.com>
2021-02-12 17:53:40 -0800

    adding problem 4

--- starter-code/app.js
+++ starter-code/app.js
@@ -90,17 +90,18 @@ Write a function called sumArray() that takes in an array of numbers as its sing
 IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
-
-// Write your code here
-let testArray = [2, 3, 4]; //eslint-disable-line
-
+let testArray = [2,3,4];
 function sumArray(sumArr) { //eslint-disable-line
+let firstSum = sum(sumArr[0], sumArr[1])[0];
+let totalSum = sum(sumArr[2], firstSum)[0];
+let wordSum = sumArr[0] + ',' + sumArr[1] + ',' + sumArr[2] + ' was passed in as an array of numbers, and ' + totalSum + ' is their sum.';
 
+return [totalSum, wordSum];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
